01dacc3d7721dd40f4e2f07176a33684ccda4e16
yonatanzxc123 <yhonatan3@gmail.com>
2026-05-20 13:55:09 +0300

    Intial IssueFlow skeleton

---
+++ src/main/java/com/att/tdp/issueflow/IssueFlowApplication.java
@@ -0,0 +1,13 @@
+package com.att.tdp.issueflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/att/tdp/issueflow/IssueFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.att.tdp.issueflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
